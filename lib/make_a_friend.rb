1f88db3367f4040d5e5a6868dd7479abc55516a3
Ewan <ewan.s@cucumberwifi.io>
2018-06-20 11:13:59 +0100

    dependency

--- lib/make_a_friend.rb
+++ lib/make_a_friend.rb
@@ -1,5 +1,6 @@
 require "make_a_friend/engine"
-# require "make_a_friend/application_controller"
+require "make_a_friend/application_controller"
+require "../app/controllers/make_a_friend/application_controller"
 
 module MakeAFriend
   class FriendsController < ApplicationController
